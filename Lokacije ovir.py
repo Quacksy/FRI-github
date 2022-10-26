bd9904d165e760a8852a0ea2ae5113a51b01060c
Kizuroy <32106420+kizuroy@users.noreply.github.com>
2022-10-26 09:26:50 +0200

    Update Lokacije ovir.py

--- Lokacije ovir.py
+++ Lokacije ovir.py
@@ -11,4 +11,5 @@ zemljevid = [
 
 zemljevid = np.array(zemljevid)
 zemljevid = zemljevid.reshape(5, 1)
+#Moremo spremenit tabelo v numericno da lahko ugotovimo lokacijo pik in lojter
 print(zemljevid)
