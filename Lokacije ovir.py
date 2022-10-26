e542b962dd04b2e63caa4a17a2b68fb567f70b41
Kizuroy <32106420+kizuroy@users.noreply.github.com>
2022-10-26 09:10:15 +0200

    V delanju........

--- Lokacije ovir.py
+++ Lokacije ovir.py
@@ -12,8 +12,3 @@ zemljevid = [
 zemljevid = np.array(zemljevid)
 zemljevid = zemljevid.reshape(5, 1)
 print(zemljevid)
-
-for _ in zemljevid:
-    for a in _:
-        for b in a:
-            print(b)
