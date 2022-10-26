669a74893f17b4c3c45b3f700f692a8491784ce5
Kizuroy <32106420+kizuroy@users.noreply.github.com>
2022-10-26 09:06:32 +0200

    Test

--- Lokacije ovir.py
+++ Lokacije ovir.py
@@ -1,3 +1,4 @@
+from turtle import right
 import numpy as np
 
 zemljevid = [
@@ -12,3 +13,7 @@ zemljevid = np.array(zemljevid)
 zemljevid = zemljevid.reshape(5, 1)
 print(zemljevid)
 
+for _ in zemljevid:
+    for a in _:
+        for b in a:
+            print(b)
