c6912156e94562a549e48bbf1a64cc70da6baf95
Kizuroy <32106420+kizuroy@users.noreply.github.com>
2022-10-26 08:34:44 +0200

    WTF se dogaja
    Test

---
+++ Lokacije ovir.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+zemljevid = [
+    "......",
+    "..##..",
+    ".##.#.",
+    "...###",
+    "###.##",
+]
+
+zemljevid = np.array(zemljevid)
+zemljevid = zemljevid.reshape(5, 1)
+print(zemljevid)
+
