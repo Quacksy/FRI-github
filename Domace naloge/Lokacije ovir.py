ff66f6e659b45478157894b3a01fe403af7f4767
Kizuroy <32106420+kizuroy@users.noreply.github.com>
2022-10-30 22:15:49 +0100

    update depresija

--- Domace naloge/Lokacije ovir.py
+++ Domace naloge/Lokacije ovir.py
@@ -1,30 +1,42 @@
-from distutils.log import set_verbosity
-from turtle import right
-from typing import overload
 import numpy as np
 
 
 #Segrevanje
-ovire = ".##..####...##"
-pregled = []
+# ovire = ".##..####...##"
+# pregled = []
+# pregled = np.array(pregled)
 
-for od in ovire:
-    for do in ovire:
-        print(od)
-        print(do)
+# for lok in enumerate(ovire,start=1):
+#     if lok[1] == "#":
+#         if lok[0 + 1] == :
+
+#         pregled = np.append(pregled,lok[0])
+
+# pregled = np.reshape(pregled, (len(pregled) // 2, 2))
+# print(pregled)
+    
 
 
 
 
-# zemljevid = [
-#     "......",
-#     "..##..",
-#     ".##.#.",
-#     "...###",
-#     "###.##",
-# ]
 
-# zemljevid = np.array(zemljevid)
-# zemljevid = zemljevid.reshape(5, 1)
-# #Moremo spremenit tabelo v numericno da lahko ugotovimo lokacijo pik in lojter
-# print(zemljevid)
+zemljevid = [
+    "......",
+    "..##..",
+    ".##.#.",
+    "...###",
+    "###.##",
+]
+
+zemljevid = np.array(zemljevid)
+zemljevid = zemljevid.reshape(len(zemljevid), 1)
+tabela = []
+#Moremo spremenit tabelo v numericno da lahko ugotovimo lokacijo pik in lojter
+stevec = 1
+for a in zemljevid:
+    for b in a:
+        for c in enumerate(b, start=1):
+            if c[1] == "#" and c[1]:
+                tabela.append([c[0], stevec])
+    stevec += 1
+print(tabela)
